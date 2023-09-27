a26f57c8671340be05b37ec6e98555867c9813cf
samuel <samuel.chicha@live.fr>
2023-09-27 21:18:22 +0200

    build: switch to .ini

--- Map.py
+++ Map.py
@@ -1,18 +1,61 @@
-import json
-from Troncon import Troncon
+import configparser
+from Section import Section
 from Point import Point
-from Croisement import Croisement
+from Light import Light
+from Graph import Graph
 
 class Map:
     def __init__(self, file_path : str) -> None:
         self.file_path = file_path
+        self.sections = None
+        self.intersections = None
 
-    def get_troncons(self) -> list:
-        with open(self.file_path) as map_file:
-            map = json.load(map_file)
-            return [Troncon(Point(troncon['xa'], troncon['ya']), Point(troncon['xb'], troncon['yb'])) for troncon in map['troncons']]
-        
-    def get_croisements(self) -> list:
-        with open(self.file_path) as map_file:
-            map = json.load(map_file)
-            return [Croisement(Point(croisement['x'], croisement['y'])) for croisement in map['croisements']]
+    def get_sections(self) -> list[Section]:
+        """Get the sections from the map file
+
+        Returns:
+            list[Section]: list of sections
+        """
+        if self.sections is None:
+            parser = configparser.ConfigParser()
+            parser.read(self.file_path)
+            self.sections = []
+            for id in parser['sections']:
+                xa, ya, xb, yb = eval(parser['sections'][id])
+                self.sections.append(Section(id, Point(xa, ya), Point(xb, yb)))
+        return self.sections
+    
+    def get_intersections(self) -> list[Light]:
+        """Get the intersections from the map file
+
+        Returns:
+            list[Intersection]: list of intersections
+        """
+        if self.intersections is None:
+            parser = configparser.ConfigParser()
+            parser.read(self.file_path)
+            self.intersections = []
+            for id in parser['trafficLights']:
+                x, y = eval(parser['trafficLights'][id])
+                self.intersections.append(Light(Point(x, y)))
+        return self.intersections
+
+    def get_graph(self) -> Graph:
+        """Transforms the map into a graph
+
+        Returns:
+            Graph: resulting graph
+        """
+        matrix = {}
+        for section in self.get_sections():
+            if section.posA.__repr__() not in matrix.keys():
+                matrix[section.posA.__repr__()] = {}
+            if section.posB.__repr__() not in matrix[section.posA.__repr__()].keys():
+                matrix[section.posA.__repr__()][section.posB.__repr__()] = {}
+            matrix[section.posA.__repr__()][section.posB.__repr__()] = abs(section.posA.y - section.posB.y if section.posA.x == section.posB.x else section.posA.x - section.posB.x)
+        return Graph(matrix)
+
+# parser = configparser.ConfigParser()
+# parser.read('map.ini')
+# for id in parser['trafficLights']:
+#     print(id)
